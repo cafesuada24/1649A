4c62162f6a59ef58d0c984a0d58361e9e8bf9f12
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-10-25 13:38:38 +0700

    Rename Sorter to SortStrategy

--- src/main/java/com/hahsm/algorithms/SortStrategy.java
+++ src/main/java/com/hahsm/algorithms/SortStrategy.java
@@ -2,7 +2,8 @@ package com.hahsm.algorithms;
 
 import java.util.List;
 
-public interface Sorter<T extends Comparable<T>> {
+public interface SortStrategy<T extends Comparable<T>> {
+
     public void sort(List<T> data);
 
     public void sort(T[] data);
