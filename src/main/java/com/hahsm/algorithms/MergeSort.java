d6a685392d59cdd9eb386eecd9d9d16614e339e2
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-10-25 14:04:06 +0700

    Minor changes for mergesort

--- src/main/java/com/hahsm/algorithms/MergeSort.java
+++ src/main/java/com/hahsm/algorithms/MergeSort.java
@@ -1,41 +1,51 @@
 package com.hahsm.algorithms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MergeSort<T extends Comparable<T>> implements Sorter<T> {
+import com.hahsm.datastructures.ArrayWrapper;
+import com.hahsm.datastructures.IndexedCollection;
+import com.hahsm.datastructures.ListWrapper;
+
+public class MergeSort<T extends Comparable<T>> implements SortStrategy<T> {
 
     @Override
     public void sort(T[] data) {
-        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+        final T[] temp = Arrays.copyOf(data, data.length);
+        sort(new ArrayWrapper<>(data), new ArrayWrapper<>(temp));
     }
 
     @Override
-    public void sort(List<T> arr) {
-        final int size = arr.size();
+    public void sort(List<T> data) {
+        final List<T> temp = new ArrayList<>(data);
+        sort(new ListWrapper<>(data), new ListWrapper<>(temp));
+    }
 
-        List<T> temp = new ArrayList<>(arr);
+    private void sort(IndexedCollection<T> source, IndexedCollection<T> temp) {
+        final int size = source.size();
+
         for (int width = 1; width < size; width <<= 1) {
             for (int index = 0; index < size; index += 2 * width) {
-                merge(arr, index, Math.min(index + width, size), Math.min(index + 2 * width, size), temp);
+                merge(source, index, Math.min(index + width, size), Math.min(index + 2 * width, size), temp);
             }
 
-            copy(temp, arr);
+            copy(temp, source);
         }
     }
 
-    private void merge(List<T> arr, int indexLeft, int indexMiddle, int indexRight,
-            List<T> temp) {
+    private void merge(IndexedCollection<T> collection, int indexLeft, int indexMiddle, int indexRight,
+            IndexedCollection<T> temp) {
         for (int i = indexLeft, j = indexMiddle, k = indexLeft; k < indexRight; ++k) {
-            if (i < indexMiddle && (j >= indexRight || arr.get(i).compareTo(arr.get(j)) == -1)) {
-                temp.set(k, arr.get(i++));
+            if (i < indexMiddle && (j >= indexRight || collection.get(i).compareTo(collection.get(j)) == -1)) {
+                temp.set(k, collection.get(i++));
             } else {
-                temp.set(k, arr.get(j++));
+                temp.set(k, collection.get(j++));
             }
         }
     };
 
-    private void copy(List<T> from, List<T> to) {
+    private void copy(IndexedCollection<T> from, IndexedCollection<T> to) {
         if (from.size() != to.size()) {
             throw new IllegalArgumentException(
                     "Inconsistent list sizes: from size = " + from.size() + ", to size = " + to.size());
