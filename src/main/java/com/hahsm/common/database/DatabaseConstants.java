0864b5c4233cf63fe05e44063035f18371bea41c
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-10-28 08:54:00 +0700

    Update database constants

---
+++ src/main/java/com/hahsm/common/database/DatabaseConstants.java
@@ -0,0 +1,29 @@
+package com.hahsm.common.database;
+
+public class DatabaseConstants {
+
+    private DatabaseConstants() {}
+
+    public static final String USER_TABLE = "users";
+    public static final String BOOK_TABLE = "books";
+    public static final String ORDER_TABLE = "orders";
+
+
+    public static final class UserColumns {
+        public static final String ID = "id";
+        public static final String NAME = "name";
+        public static final String ADDRESS = "address";
+    }
+
+    public static final class BookColumns {
+        public static final String ID = "id";
+        public static final String TITLE = "title";
+        public static final String AUTHOR = "author";
+    }
+    
+    public static final class OrderColumns {
+        public static final String USER_ID = "user_id";
+        public static final String BOOK_ID = "book_id";
+        public static final String CREATED_AD = "created_at";
+    }
+}
