8b7e78c2d19bcd9f8ead4a937a9f370fc85ad94a
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-05 00:27:49 +0700

    Update algorithm

--- src/main/java/com/hahsm/algorithm/Search.java
+++ src/main/java/com/hahsm/algorithm/Search.java
@@ -5,9 +5,9 @@ import java.util.Comparator;
 import com.hahsm.datastructure.adt.List;
 
 public interface Search {
-    //<T> int search(List<T> data, Object target);
-
     <T extends Comparable<T>> int search(List<T> data, T target);
+
+    <T> int search(List<T> data, Object target);
 
     <T> int search(List<T> data, T target, Comparator<T> comparator);
 }
