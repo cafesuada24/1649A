433cd63f1728c70922f2fce80afb62adea460366
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-04 21:40:35 +0700

    Update Search algos

--- src/main/java/com/hahsm/algorithm/LinearSearch.java
+++ src/main/java/com/hahsm/algorithm/LinearSearch.java
@@ -1,6 +1,7 @@
 package com.hahsm.algorithm;
 
 import java.util.Comparator;
+import java.util.function.Function;
 
 import com.hahsm.datastructure.ArrayList;
 import com.hahsm.datastructure.adt.List;
@@ -17,16 +18,43 @@ public class LinearSearch implements Search {
         return -1;
     }
 
+    
+
     @Override
     public <T> List<Integer> searchAll(List<T> data, T target, Comparator<T> comparator) {
-        List<Integer> found = new ArrayList<>(data.size());
+        List<Integer> indexes = new ArrayList<Integer>(data.size());
 
         for (int i = 0; i < data.size(); ++i) {
             if (comparator.compare(data.get(i), target) == 0) {
-                found.add(i);
+                indexes.add(i);
             }
         }
 
-        return found;
+        return indexes;
     }
+
+
+
+	@Override
+	public <T> int search(List<T> data, Function<T, Integer> func) {
+        for (int i = 0; i < data.size(); ++i) {
+            if (func.apply(data.get(i)) == 0) {
+                return i;
+            }
+        }
+        return -1;
+	 }
+
+
+
+	@Override
+	public <T> List<Integer> searchAll(List<T> data, Function<T, Integer> func) {
+        List<Integer> indexes = new ArrayList<>(data.size());
+        for (int i = 0; i < data.size(); ++i) {
+            if (func.apply(data.get(i)) == 0) {
+                indexes.add(i);
+            }
+        }
+        return indexes;
+	}
 }
--- src/main/java/com/hahsm/algorithm/Search.java
+++ src/main/java/com/hahsm/algorithm/Search.java
@@ -1,6 +1,7 @@
 package com.hahsm.algorithm;
 
 import java.util.Comparator;
+import java.util.function.Function;
 
 import com.hahsm.datastructure.adt.List;
 
@@ -10,10 +11,12 @@ public interface Search {
     }
 
     <T> int search(List<T> data, T target, Comparator<T> comparator);
+    <T> int search(List<T> data, Function<T, Integer> func);
 
     default <T extends Comparable<T>> List<Integer> searchAll(List<T> data, T target) {
         return searchAll(data, target, Comparator.naturalOrder());
     }
 
     <T> List<Integer> searchAll(List<T> data, T target, Comparator<T> comparator);
+    <T> List<Integer> searchAll(List<T> data, Function<T, Integer> func);
 }
