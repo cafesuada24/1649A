ca54d4decd162cd3e59e22816652be6d72c664d0
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-11-13 22:39:14 +0700

    Update

--- src/main/java/com/hahsm/cart/BookCart.java
+++ src/main/java/com/hahsm/cart/BookCart.java
@@ -1,26 +1,23 @@
 package com.hahsm.cart;
 
-import java.sql.Date;
-import java.sql.Time;
 import java.time.LocalDateTime;
 
 import com.hahsm.algorithm.SortStrategy;
 import com.hahsm.book.model.Book;
 import com.hahsm.common.type.Pair;
+import com.hahsm.common.type.User;
 import com.hahsm.datastructure.HashMap;
-import com.hahsm.datastructure.adt.List;
 import com.hahsm.order.model.Order;
 import com.hahsm.order.model.OrderBook;
 
 public class BookCart {
     private HashMap<Integer, Pair<Integer, Book>> cart = new HashMap<>();
-    private String customerName;
-    private String customerAddress;
-    private String customerPhone;
+    private User user;
     private SortStrategy sorter;
 
-    public BookCart(SortStrategy sorter) {
+    public BookCart(final SortStrategy sorter, final User user) {
         this.sorter = sorter;
+        this.user = user;
     }
 
     public void addItem(int id, Book book, int qty) {
@@ -44,24 +41,22 @@ public class BookCart {
         items.forEach((item) -> {
             builder.append("\t").append(item.getSecond().toString());
             builder.append("\n\t\tqty: ").append(item.getFirst()).append('\n');
-        });;
+        });
+
         return builder.toString();
     }
 
-    public void setCustomerInformation(String customerName, String customerAddress, String customerPhone) {
-        this.customerName = customerName;
-        this.customerAddress = customerAddress;
-        this.customerPhone = customerPhone;
-    }
-
     public Order toOrder() {
+        if (user == null) {
+            throw new IllegalStateException("User must be set");
+        }
         final Order order = new Order();
         final var now = LocalDateTime.now();
         order.setOrderTime(now);
         order.setEstimatedDeliveryTime(now.plusSeconds(30));
-        order.setCustomerName(customerName);
-        order.setCustomerAddress(customerAddress);
-        order.setCustomerPhone(customerPhone);
+        order.setCustomerName(user.name);
+        order.setCustomerAddress(user.address);
+        order.setCustomerPhone(user.phone);
         order.setStatus(Order.Status.PROCESSING);
 
         for (final var entry : cart) {
---
+++ src/main/java/com/hahsm/common/type/User.java
@@ -0,0 +1,14 @@
+package com.hahsm.common.type;
+
+
+public class User {
+    public String name;
+    public String address;
+    public String phone;
+
+    public User(final String name, final String address, final String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+}
